f7f74af636d6f702ebf75c70f79a6096b95bbbc9
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-11-11 02:34:07 +0530

    progress so far

--- frame.py
+++ frame.py
@@ -3,11 +3,23 @@ import numpy as np
 from vector import Vector
 
 
+class Sphere:
+    def __init__(self, centre, radius) -> None:
+        self.centre = centre
+        self.radius = radius
+
+
 class Frame:
     def __init__(self, height, width) -> None:
         self.height = height
         self.width = width
         self.frame = np.zeros(shape=(height, width, 3))
+
+    def canvas_to_viewport(self, x, y):
+        canvas_w = 100
+        canvas_h = 100
+        distance = 1
+        return x * self.width / canvas_w, y * self.height / canvas_h, distance
 
     def render(self):
         origin = Vector(0, 0, 0)
@@ -15,37 +27,28 @@ class Frame:
         for x in range(- self.width, self.width + 1):
             for y in range(- self.height, self.height + 1):
                 coordinate = self.canvas_to_viewport(x, y)
-                color = trace_ray(origin, coordinate, 1, np.inf)
+                color = trace_ray(origin, coordinate, t_min=1, t_max=np.inf)
                 self.frame[x, y] = color
 
-    def canvas_to_viewport(self, x, y):
-        canvas_w = 100
-        canvas_h = 100
-        distance = 1
 
-        return x * self.width / canvas_w, y * self.height / canvas_h, distance
-
-
-def trace_ray(start, d, end, inf):
-    BACKGROUND_COLOR = (0, 0, 0)
+def trace_ray(start, coordinate, t_min, t_max):
+    BACKGROUND_COLOR = [0, 0, 0]
     closest_t = np.inf
     closest_sphere = None
     scene = None
-    t_min = 0
-    t_max = 0
 
     for sphere in scene:
-        t1, t2 = get_intersection(start, d, sphere)
+        t1, t2 = get_intersection(start, coordinate, sphere)
 
-        if t1 in [t_min, t_max] and t1 < closest_t:
+        if t1 > t_min and t1 < t_max and t1 < closest_t:
             closest_t = t1
             closest_sphere = sphere
 
-        if t2 in [t_min, t_max] and t2 < closest_t:
+        if t2 > t_min and t2 < t_max and t2 < closest_t:
             closest_t = t2
             closest_sphere = sphere
 
-        if closest_sphere == None:
+        if closest_sphere is None:
             return BACKGROUND_COLOR
 
         return closest_sphere.color
@@ -53,11 +56,11 @@ def trace_ray(start, d, end, inf):
 
 def get_intersection(start, D, sphere):
     r = sphere.radius
-    CO = start - sphere.center
+    c_o_vector = start - sphere.center
 
     a = Vector(D) * Vector(D)
-    b = 2 * Vector(CO) * Vector(D)
-    c = Vector(CO) * Vector(CO) - r * r
+    b = 2 * Vector(c_o_vector) * Vector(D)
+    c = Vector(c_o_vector) * Vector(c_o_vector) - r * r
 
     discriminant = b * b - 4 * a * c
 
--- vector.py
+++ vector.py
@@ -2,10 +2,10 @@ import numpy as np
 
 
 class Vector:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, v):
+        self.x = v[0]
+        self.y = v[1]
+        self.z = v[2]
 
     def __repr__(self):
         return str((self.x, self.y, self.z))
@@ -13,8 +13,14 @@ class Vector:
     def __array__(self):
         return np.array([self.x, self.y, self.z])
 
+    def __add__(self, other):
+        return Vector(np.array(self) + np.array(other))
+
+    def __sub__(self, other):
+        return Vector(np.array(self) - np.array(other))
+
     def __mul__(self, other):
-        return np.array(self) * np.array(other)
+        return Vector(np.array(self) * np.array(other))
 
     def __rmul__(self, other):
         return self.__mul__(other)
