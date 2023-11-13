bfab33427f357779e55991418dccec4fb0f879e9
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-11-14 00:12:38 +0530

    cleaned up a bit

--- frame.py
+++ frame.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+BACKGROUND_COLOR = [1, 1, 1]
+
+
 class Sphere:
     def __init__(self, centre, radius, color):
         self.centre = centre
@@ -13,14 +16,24 @@ class Canvas:
     def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.distance = 1
+
+        # FOV ~ 53 degree
+        self.viewport_width = 1
+        self.viewport_height = 1
+        self.viewport_distance = 1
+
+        self.scene = []
         self.frame = np.zeros(shape=(height, width, 3))
 
+    def add_sphere(self, centre, radius, color):
+        self.scene.append(Sphere(centre, radius, color))
+
     def canvas_to_viewport(self, x, y):
-        width = 1
-        height = 1
-        distance = 1
-        return np.array([x * width / self.width, y * height / self.height, distance])
+        vx = x * self.viewport_width / self.width
+        vy = y * self.viewport_height / self.height
+        vz = self.viewport_distance
+
+        return np.array([vx, vy, vz])
 
     def render(self):
         origin = np.array([0, 0, 0])
@@ -28,7 +41,13 @@ class Canvas:
         for x in range(- self.width // 2, self.width // 2):
             for y in range(- self.height // 2, self.height // 2):
                 pixel_pos = self.canvas_to_viewport(x, y)
-                color = trace_ray(origin, pixel_pos, t_min=self.distance, t_max=np.inf)
+                color = trace_ray(
+                    scene=self.scene,
+                    origin=origin,
+                    pixel_pos=pixel_pos,
+                    t_min=self.viewport_distance,
+                    t_max=np.inf
+                )
                 self.frame[x + self.width // 2, y + self.height // 2] = color
 
         fig = plt.figure(figsize=(5, 5))
@@ -40,18 +59,12 @@ class Canvas:
         # return np.rot90(self.frame)
 
 
-def trace_ray(start, pixel_pos, t_min, t_max):
-    BACKGROUND_COLOR = [1, 1, 1]
+def trace_ray(scene, origin, pixel_pos, t_min, t_max):
     closest_t = np.inf
     closest_sphere = None
-    scene = [
-        Sphere([0, -1, 3], 1, [1, 0, 0]),
-        Sphere([2, 0, 4], 1, [0, 1, 0]),
-        Sphere([-2, 0, 4], 1, [0, 0, 1])
-    ]
 
     for sphere in scene:
-        t1, t2 = get_intersection(start, pixel_pos, sphere)
+        t1, t2 = get_intersection(origin, pixel_pos, sphere)
 
         if t1 > t_min and t1 < t_max and t1 < closest_t:
             closest_t = t1
@@ -69,11 +82,11 @@ def trace_ray(start, pixel_pos, t_min, t_max):
 
 def get_intersection(start, pixel_pos, sphere):
     r = sphere.radius
-    c_o_vector = start - sphere.centre
+    ray_vector = start - sphere.centre
 
     a = (pixel_pos * pixel_pos).sum()
-    b = 2 * np.dot(c_o_vector,  pixel_pos)
-    c = np.dot(c_o_vector, c_o_vector) - r * r
+    b = 2 * np.dot(ray_vector,  pixel_pos)
+    c = np.dot(ray_vector, ray_vector) - r * r
 
     discriminant = (b * b - 4 * a * c)
 
@@ -88,4 +101,8 @@ def get_intersection(start, pixel_pos, sphere):
 
 if __name__ == "__main__":
     canvas = Canvas(height=200, width=200)
+    canvas.add_sphere(centre=[0, -1, 3], radius=1, color=[1, 0, 0])
+    canvas.add_sphere(centre=[2, 0, 4], radius=1, color=[0, 1, 0])
+    canvas.add_sphere(centre=[-2, 0, 4], radius=1, color=[0, 0, 1])
+
     canvas.render()
